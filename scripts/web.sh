d6f3be22056251e7382fd260c5e1a7f50a0360a6
zouqilin <mr.zouqilin@gmail.com>
2017-04-30 15:31:00 +0800

    set rvm ruby_url to ruby-china

--- scripts/web.sh
+++ scripts/web.sh
@@ -2,6 +2,8 @@
 apt-get install -y libmysqlclient20 libmyclient-dev ruby2.3 ruby2.3-dev
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
+mkdir -p ~/.rvm/user
+echo "ruby_url=https://cache.ruby-china.org/pub/ruby" > ~/.rvm/user/db
 
 source /etc/profile.d/rvm.sh
 rvm reload
