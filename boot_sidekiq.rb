56443a00e6e7383b25bfc1cc70e62b5064cff967
zouqilin <mr.zouqilin@gmail.com>
2017-05-13 11:44:16 +0800

    rename Vagrantfile to Vagrantfile.example

--- boot_sidekiq.rb
+++ boot_sidekiq.rb
@@ -1,7 +1,7 @@
 require "./boot"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORTPORT"]}" }
+  config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}" }
 end
 
 Sidekiq.configure_client do |config|
